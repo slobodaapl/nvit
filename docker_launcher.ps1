d76b36355211686dbc77a611d7b7aa8a9bd00dd3
slobodaapl <slobodaapl@gmail.com>
2024-11-23 10:25:17 +0100

    Updated launchers

--- docker_launcher.ps1
+++ docker_launcher.ps1
@@ -1,12 +1,24 @@
-# Read number of GPUs from args on command line, if present, otherwise default to 1
+# Default values
 param(
-    [int]$num_gpus = 1
+    [int]$num_gpus = 1,
+    [string]$wandb_token = ""
 )
+
+# Load environment variables from .env file if it exists
+if (Test-Path .env) {
+    Get-Content .env | ForEach-Object {
+        if ($_ -match '^([^=]+)=(.*)$') {
+            [Environment]::SetEnvironmentVariable($matches[1], $matches[2])
+        }
+    }
+}
 
 # Run docker container with local directory mounted and execute training command
 docker run --rm `
     --gpus all `
     -v ${PWD}:/app `
     -w /app `
+    -e WANDB_API_KEY=$wandb_token `
+    --env-file .env `
     nvit:latest `
     torchrun --nnodes 1 --nproc_per_node $num_gpus --rdzv_endpoint=localhost:29501 nvit/train.py
--- docker_launcher.sh
+++ docker_launcher.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
-# read number of GPUs from args on command line, if present, otherwise default to 8
-num_gpus=$1
+# Default values
+num_gpus=1
+wandb_token=""
 
-if [ -z "$num_gpus" ]; then
-    num_gpus=1
+# Load environment variables from .env file if it exists
+if [ -f .env ]; then
+    export $(cat .env | xargs)
 fi
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --num_gpus)
+            num_gpus="$2"
+            shift 2
+            ;;
+        --wandb_token)
+            wandb_token="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            exit 1
+            ;;
+    esac
+done
 
 # Run docker container with local directory mounted and execute training command
 docker run --rm \
     --gpus all \
     -v $(pwd):/app \
     -w /app \
+    -e WANDB_API_KEY=$wandb_token \
+    --env-file .env \
     nvit:latest \
     torchrun --nnodes 1 --nproc_per_node $num_gpus --rdzv_endpoint=localhost:29501 nvit/train.py
