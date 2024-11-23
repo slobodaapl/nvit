c60742f682aab174f7fe9135ca398cade2384e1e
slobodaapl <slobodaapl@gmail.com>
2024-11-23 13:35:48 +0100

    Ignoring prev installed nvit

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@
 set -xeuo pipefail
 
 # Install the mounted project in editable mode, for both development and running
-pip install --no-deps --user --root-user-action ignore -e .
+pip install --upgrade --no-deps --user --root-user-action ignore -e .
 
 # Execute passed command
 exec "$@"
